2ba64b04a8c40c05698f072f87596b93286add6d
Euan Goddard <euan@stockopedia.com>
2021-01-17 20:08:01 +0000

    chore(ui): Added help copy for defining problems

--- src/Define.svelte
+++ src/Define.svelte
@@ -1,14 +1,46 @@
+<style>
+  .relative {
+    position: relative;
+  }
+  .help-button {
+    appearance: none;
+    background-color: #333;
+    width: 2rem;
+    height: 2rem;
+    font-size: 1rem;
+    color: #f4f4f4;
+    border-radius: 50%;
+    padding: 0;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+
+  code {
+    font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier,
+      monospace;
+    display: inline-block;
+    border: 1px solid var(--heading-color);
+    color: var(--heading-color);
+    background-color: var(--invalid-color);
+    padding: 0 0.25rem;
+  }
+</style>
+
 <script lang="ts">
   import { ParseError, parseProblemDef } from "./parse";
   import type { Problems } from "./models";
   import { ProblemTokenType } from "./models";
   import { createEventDispatcher } from "svelte";
+  import { slide } from "svelte/transition";
 
   let problemDef: string =
     "1: 10 x ~ = 100\n\n2: 7 lots of ~ = 56\n~ multiplied by 8 = 56";
   let isValid = true;
   let errorMessage: string | null = null;
   let problems: Problems;
+
+  let isHelpOpen = false;
 
   $: {
     try {
@@ -29,10 +61,28 @@
   const saveProblem = () => {
     dispatch("save", { problems, problemDef });
   };
+
+  const toggleHelp = () => {
+    isHelpOpen = !isHelpOpen;
+  };
 </script>
 
 <main>
-  <h1>Enter problem</h1>
+  <h1 class="relative">
+    Enter problem <button
+      class="help-button"
+      type="button"
+      on:click="{toggleHelp}">?</button
+    >
+  </h1>
+  {#if isHelpOpen}
+    <p transition:slide>
+      <small class="help"
+        >To denote an answer box use a <code>~</code>. Leave a blank line
+        between each question.</small
+      >
+    </p>
+  {/if}
   <textarea bind:value="{problemDef}" class:invalid="{!isValid}"></textarea>
   <button type="button" disabled="{!isValid}" on:click="{saveProblem}"
     >Save</button
